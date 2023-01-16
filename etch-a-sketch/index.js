79912f2102a5307abf1685109a82dc949841954c
Charlie <cmefron@gmail.com>
2023-01-16 08:55:57 -0800

    program paint functions, add color picker, change styles

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -1,38 +1,65 @@
-let numOfSquares = 25;
+let numOfSquares = 100;
 
-const gridContainer = document.getElementById("grid-container");
+const grid = document.getElementById("grid");
 
 function populateGrid() {
     for (let i = 0; i < numOfSquares; i++){
         const square = document.createElement('div');
         square.classList.add("square");
         square.style.width = `${(100/(Math.sqrt(numOfSquares))) + "%"}`;
-        gridContainer.appendChild(square);
+        grid.appendChild(square);
     }
 }
 
 populateGrid();
 
 const squares = document.querySelectorAll(".square");
-
-
-
-//squares.forEach(square=>
-  //square.addEventListener("click", changeColor));
     
-
+let mouseDown = false;
 
 for (let i = 0; i < numOfSquares; i++){
-        squares[i].addEventListener("click", changeColor(i));
-    }
+        squares[i].addEventListener("mousedown", function(){
+        mouseDown = true;
+        console.log("mousedown");
+    })    
+}
 
-let colorSelection = "blue";
+document.addEventListener("mouseup", function(){
+    mouseDown = false;
+    console.log("mouseup");
+    })    
 
-function changeColor(i){
+// Changes square colors when you drag mouse over them
+function paint(i){
     return function(){
+        if (mouseDown){
         squares[i].style.backgroundColor = colorSelection;
-    }
-};
+        }
+    };
+}
+
+for (let i = 0; i < numOfSquares; i++){    
+    squares[i].addEventListener("mouseover", paint(i));
+}
+
+for (let i = 0; i < numOfSquares; i++){    
+    squares[i].addEventListener("mousedown", paint(i));
+}
+
+
+let colorSelection = "#FF0000"; // Default color is red
+
+const colorPicker = document.getElementById("color-picker");
+
+// Gets color from color pickeer
+colorPicker.addEventListener("input", function(){
+    colorSelection = colorPicker.value;
+    console.log(colorPicker.value);
+    }, false);
+
+
+
+
 
 
 
